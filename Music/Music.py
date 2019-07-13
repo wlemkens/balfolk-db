5b9201b1b42a0f63fe3f1835d68601307d387279
Wim Lemkens <wim@avoid.be>
2019-07-13 19:44:26 +0200

    Storing to database seems to work, but is not properly tested

--- Music/Music.py
+++ Music/Music.py
@@ -5,21 +5,23 @@ class Band(object):
         self.name = name
 
 class Album(object):
-    def __init__(self, band, name, year):
+    def __init__(self, band, name, year, nb_tracks):
         self.id = None
         self.band = band
         self.name = name
         self.year = year #
+        self.nb_tracks = nb_tracks
         self.cddb_id = None
 
 class Track(object):
-    def __init__(self, album, number, title, dances):
+    def __init__(self, album, number, title, dances, band):
         self.id = None
         self.album = album
         self.number = number
         self.title = title
         self.level = None
         self.dances = dances
+        self.band = band
 
 class Dance(object):
     def __init__(self, language, name):
--- tools/music_to_db.py
+++ tools/music_to_db.py
@@ -1,21 +1,36 @@
 import os
 import sys
 import mutagen
-from mutagen.id3 import ID3
+import mysql.connector
 
 from Music.Music import *
 
 def parse_dance(dance):
     return dance[0].replace("Folk ", "")
 
+def getYear(date):
+    if isinstance(date,mutagen.id3.ID3TimeStamp):
+        return date.year
+    if len(date) > 4:
+        parts = date.split("-")
+        if len(parts[0]) == 4:
+            return int(parts[0])
+        else:
+            return int(parts[2])
+    return int(date)
+
 def extract_v1(file):
     artist = None
+    albumartist = None
     if "artist" in file.keys():
         artist = file["artist"][0]
     if "albumartist" in file.keys():
-        artist = file["albumartist"][0]
+        albumartist = file["albumartist"][0]
     if artist and "genre" in file.keys():
         band = Band(artist)
+        albumband = None
+        if albumartist:
+            albumband = Band(albumartist)
         language = Language("Nederlands")
         genre = None
         if "genre" in file.keys():
@@ -23,13 +38,19 @@ def extract_v1(file):
         dance = Dance(language, genre)
         year = None
         if "year" in file:
-            year = file["year"][0]
+            year = getYear(file["year"][0])
         elif "date" in file:
-            year = file["date"][0]
+            year = getYear(file["date"][0])
         album = None
         if "album" in file.keys():
-            album = Album(band, file["album"][0], year)
-        track = Track(album, file["tracknumber"][0], file["title"][0], [dance])
+            total_tracks = -1
+            if "totaltracks" in file.keys():
+                total_tracks = file["totaltracks"][0]
+            album = Album(albumband, file["album"][0], year, total_tracks)
+        track_nb = -1
+        if "tracknumber" in file.keys():
+            track_nb = file["tracknumber"][0]
+        track = Track(album, track_nb, file["title"][0], [dance], band)
         if "cddb-id" in file.keys():
             album.cddb_id = file["cddb-id"][0]
         return track
@@ -46,7 +67,7 @@ def extract_v2(file):
     required_tags = ["TPE1", "TIT2"]
     #print(file)
     if hasTags(file, required_tags):
-        artist = file["TPE1"].text
+        artist = file["TPE1"].text[0]
         band = Band(artist)
         language = Language("Nederlands")
         genre = None
@@ -54,15 +75,18 @@ def extract_v2(file):
             genre = parse_dance(file["TCON"].text)
         dance = Dance(language, genre)
         year = None
-        if "TDRC" in file:
-            year = file["TDRC"].text
+        if "TDRC" in file.keys():
+            year = getYear(file["TDRC"].text[0])
         album = None
         if "TALB" in file.keys():
-            album = Album(band, file["TALB"].text, year)
-        track_nb = None
+            total_tracks = -1
+            # if len(file["totaltracks"]) > 0:
+            #     total_tracks = file["totaltracks"][0]
+            album = Album(band, file["TALB"].text[0], year, total_tracks)
+        track_nb = -1
         if "TRCK" in file:
-            track_nb = file["TRCK"].text
-        track = Track(album, track_nb, file["TIT2"].text, [dance])
+            track_nb = file["TRCK"].text[0]
+        track = Track(album, track_nb, file["TIT2"].text[0], [dance], band)
         if "cddb-id" in file:
             album.cddb_id = file["cddb-id"]
         return track
@@ -87,10 +111,214 @@ def extract_info_from_collection(directory):
             track = extract_info_from_file(os.path.join(dirName, fname))
             if track:
                 db += [track]
+    print ("Found {:} dances".format(len(db)))
     return db
 
-def export_to_db(db):
-    print ("Found {:} dances".format(len(db)))
+def login_to_db():
+    login = None
+    password = None
+    url = None
+    with open("login.db") as f:
+        i = 0
+        for line in f:
+            if i == 0:
+                url = line.replace("\n","")
+            if i == 1:
+                login = line.replace("\n","")
+            if i == 2:
+                password = line.replace("\n","")
+            if i == 3:
+                database = line.replace("\n","")
+            i += 1
+    mydb = None
+    if password:
+        mydb = mysql.connector.connect(host = url, password = password, user = login, database = database)
+        print("Logged in")
+    else:
+        print("No database login data")
+    return mydb
+
+def unflatten_data(flat_data):
+    bands = {}
+    albums = {}
+    languages = {}
+    dances = {}
+    tracks = {}
+    for flat_track in flat_data:
+        if flat_track.album:
+            if flat_track.album.band:
+                if not flat_track.album.band.name in bands.keys():
+                    bands[flat_track.album.band.name] = flat_track.album.band
+                    track_id = flat_track.album.band.name+"/"+flat_track.album.name+"/"+flat_track.title
+                    tracks[track_id] = flat_track
+                else:
+                    flat_track.album.band = bands[flat_track.album.band.name]
+            else:
+                track_id = flat_track.band.name+"/"+flat_track.title
+                tracks[track_id] = flat_track
+
+            if not flat_track.album.name in albums:
+                albums[flat_track.album.name] = flat_track.album
+            else:
+                flat_track.album = albums[flat_track.album.name]
+            for dance in flat_track.dances:
+                if not dance.name in dances:
+                    dances[dance.name] = dance
+                    if dance.language:
+                        if not dance.language.name in languages:
+                            languages[dance.language.name] = dance.language
+                        else:
+                            dance.language = languages[dance.language.name]
+                else:
+                    dance = dances[dance.name]
+    return bands, albums, languages, dances, tracks
+
+def store_language(language, db):
+    if not language.id:
+        cursor = db.cursor()
+
+        sql = "SELECT * FROM languages WHERE name = %s"
+        val = (language.name)
+        cursor.execute(sql, val)
+        result = cursor.fetchall()
+        if len(result) == 0:
+            sql = "INSERT INTO languages (name) VALUES (%s)"
+            cursor.execute(sql, val)
+            db.commit()
+            language.id = cursor.lastrowid
+        else:
+            language.id = result[0].id
+
+def store_dance(dance, db):
+    cursor = db.cursor()
+    if not dance.id:
+        sql = "SELECT * FROM dances WHERE levenshtein('name', %s) <= %s"
+        val = (dance.name, len(dance.name)*0.3)
+        cursor.execute(sql, val)
+        result = cursor.fetchall()
+        if len(result) == 0:
+            store_language(dance.language, db)
+            sql = "INSERT INTO dances (name, languageid) VALUES (%s, %s)"
+            val = (dance.name, dance.language.id)
+            cursor.execute(sql, val)
+            db.commit()
+            dance.id = cursor.lastrowid
+
+
+
+def update_track(track, db):
+    '''
+    Updating tracks
+    adding dances to the list
+    :param track:
+    :param db:
+    :return:
+    '''
+    cursor = db.cursor()
+
+    for dance in track.dances:
+        store_dance(dance, db)
+
+        sql = "SELECT * FROM tracks_dances WHERE trackid = %s AND danceid = %s"
+        val = (track.id, dance.id)
+        cursor.execute(sql, val)
+        result = cursor.fetchall()
+        if len(result) == 0:
+            sql = "INSERT INTO tracks_dances (trackid, danceid) VALUES (%s, %s)"
+            cursor.execute(sql, val)
+            db.commit()
+            dance.id = cursor.lastrowid
+
+def is_similar(track, db):
+    cursor = db.cursor()
+    sql = "SELECT * FROM tracks WHERE levenshtein('title', %s) <= %s"
+    val = (track.title, len(track.title)*0.2)
+    cursor.execute(sql, val)
+    result = cursor.fetchall()
+    if len(result) > 0:
+        track.id = result[0].id
+        return True
+    return False
+
+def store_band(band, db):
+    if band and not band.id:
+        cursor = db.cursor()
+        sql = "SELECT * FROM bands WHERE levenshtein('name', %s) <= %s"
+        val = (band.name, len(band.name)*0.2)
+        cursor.execute(sql, val)
+        result = cursor.fetchall()
+        if len(result) > 0:
+            band.id = result[0].id
+        else:
+            sql = "INSERT INTO bands (name, description) VALUES (%s, %s)"
+            val = (band.name, "")
+            cursor.execute(sql, val)
+            db.commit()
+            band.id = cursor.lastrowid
+
+
+
+def store_album(album, db):
+    store_band(album.band, db)
+
+    if not album.id:
+        cursor = db.cursor()
+        sql = "SELECT * FROM albums WHERE levenshtein('name', %s) <= %s"
+        val = (album.name, len(album.name)*0.2)
+        cursor.execute(sql, val)
+        result = cursor.fetchall()
+        if len(result) > 0:
+            album.id = result[0].id
+        else:
+            if album.band:
+                sql = "INSERT INTO albums (name, bandid, year, nb_tracks) VALUES (%s, %s, %s, %s)"
+                val = (album.name, album.band.id, album.year, album.nb_tracks)
+                cursor.execute(sql, val)
+                db.commit()
+                album.id = cursor.lastrowid
+            else:
+                sql = "INSERT INTO albums (name, year, nb_tracks) VALUES (%s, %s, %s)"
+                val = (album.name, album.year, album.nb_tracks)
+                cursor.execute(sql, val)
+                db.commit()
+                album.id = cursor.lastrowid
+
+def insert_track(track, db):
+    store_album(track.album, db)
+
+    if not track.id:
+        cursor = db.cursor()
+        sql = "SELECT * FROM tracks WHERE levenshtein('title', %s) <= %s"
+        val = (track.title, len(track.title)*0.2)
+        cursor.execute(sql, val)
+        result = cursor.fetchall()
+        if len(result) > 0:
+            track.id = result[0].id
+        else:
+            sql = "INSERT INTO tracks (title, albumid, number) VALUES (%s, %s, %s)"
+            print ("track:",track.title, track.album.id, track.number)
+            val = (track.title, track.album.id, track.number)
+            cursor.execute(sql, val)
+            db.commit()
+            track.id = cursor.lastrowid
+
+
+def store_track(track, db):
+    if track.id:
+        update_track(track, db)
+    elif is_similar(track, db):
+        update_track(track, db)
+    else:
+        insert_track(track, db)
+
+def export_to_db(data):
+    bands, albums, languages, dances, tracks = unflatten_data(data)
+    db = login_to_db()
+
+    for track in tracks.values():
+        store_track(track, db)
+
+
 
 if __name__ == "__main__":
     db = extract_info_from_collection(sys.argv[1])
