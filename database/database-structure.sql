1e5803bb2bb84bfed93fb717f5a7e8969cecf488
Wim Lemkens <wim@avoid.be>
2019-07-14 11:29:44 +0200

    Added sql to create the database

---
+++ database/database-structure.sql
@@ -0,0 +1,230 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 14, 2019 at 11:23 AM
+-- Server version: 10.0.38-MariaDB-0ubuntu0.16.04.1
+-- PHP Version: 7.0.33-0ubuntu0.16.04.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `c9balfolk`
+--
+CREATE DATABASE IF NOT EXISTS `c9balfolk` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `c9balfolk`;
+
+DELIMITER $$
+--
+-- Functions
+--
+DROP FUNCTION IF EXISTS `levenshtein`$$
+CREATE DEFINER=`wim`@`localhost` FUNCTION `levenshtein` (`s1` VARCHAR(255), `s2` VARCHAR(255)) RETURNS INT(11) BEGIN
+        DECLARE s1_len, s2_len, i, j, c, c_temp, cost INT;
+        DECLARE s1_char CHAR;
+                DECLARE cv0, cv1 VARBINARY(256);
+
+        SET s1_len = CHAR_LENGTH(s1), s2_len = CHAR_LENGTH(s2), cv1 = 0x00, j = 1, i = 1, c = 0;
+
+        IF s1 = s2 THEN
+            RETURN 0;
+        ELSEIF s1_len = 0 THEN
+            RETURN s2_len;
+        ELSEIF s2_len = 0 THEN
+            RETURN s1_len;
+        ELSE
+            WHILE j <= s2_len DO
+                SET cv1 = CONCAT(cv1, UNHEX(HEX(j))), j = j + 1;
+            END WHILE;
+            WHILE i <= s1_len DO
+                SET s1_char = SUBSTRING(s1, i, 1), c = i, cv0 = UNHEX(HEX(i)), j = 1;
+                WHILE j <= s2_len DO
+                    SET c = c + 1;
+                    IF s1_char = SUBSTRING(s2, j, 1) THEN
+                        SET cost = 0; ELSE SET cost = 1;
+                    END IF;
+                    SET c_temp = CONV(HEX(SUBSTRING(cv1, j, 1)), 16, 10) + cost;
+                    IF c > c_temp THEN SET c = c_temp; END IF;
+                    SET c_temp = CONV(HEX(SUBSTRING(cv1, j+1, 1)), 16, 10) + 1;
+                    IF c > c_temp THEN
+                        SET c = c_temp;
+                    END IF;
+                    SET cv0 = CONCAT(cv0, UNHEX(HEX(c))), j = j + 1;
+                END WHILE;
+                SET cv1 = cv0, i = i + 1;
+            END WHILE;
+        END IF;
+        RETURN c;
+    END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `albums`
+--
+
+DROP TABLE IF EXISTS `albums`;
+CREATE TABLE `albums` (
+  `id` int(11) NOT NULL,
+  `bandid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `cddb-id` char(8) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `nb_tracks` int(11) DEFAULT NULL,
+  `cover_art` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bands`
+--
+
+DROP TABLE IF EXISTS `bands`;
+CREATE TABLE `bands` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dances`
+--
+
+DROP TABLE IF EXISTS `dances`;
+CREATE TABLE `dances` (
+  `id` int(11) NOT NULL,
+  `languageid` int(11) NOT NULL,
+  `nameid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `level` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `languages`
+--
+
+DROP TABLE IF EXISTS `languages`;
+CREATE TABLE `languages` (
+  `id` int(11) NOT NULL,
+  `languageid` int(11) DEFAULT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tracks`
+--
+
+DROP TABLE IF EXISTS `tracks`;
+CREATE TABLE `tracks` (
+  `id` int(11) NOT NULL,
+  `albumid` int(11) DEFAULT NULL,
+  `bandid` int(11) NOT NULL,
+  `number` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tracks_dances`
+--
+
+DROP TABLE IF EXISTS `tracks_dances`;
+CREATE TABLE `tracks_dances` (
+  `trackid` int(11) NOT NULL,
+  `danceid` int(11) NOT NULL,
+  `votecount` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `albums`
+--
+ALTER TABLE `albums`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bands`
+--
+ALTER TABLE `bands`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dances`
+--
+ALTER TABLE `dances`
+  ADD PRIMARY KEY (`nameid`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `languages`
+--
+ALTER TABLE `languages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tracks`
+--
+ALTER TABLE `tracks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tracks_dances`
+--
+ALTER TABLE `tracks_dances`
+  ADD PRIMARY KEY (`trackid`,`danceid`),
+  ADD KEY `trackid` (`trackid`),
+  ADD KEY `danceid` (`danceid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `albums`
+--
+ALTER TABLE `albums`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `bands`
+--
+ALTER TABLE `bands`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `dances`
+--
+ALTER TABLE `dances`
+  MODIFY `nameid` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `languages`
+--
+ALTER TABLE `languages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tracks`
+--
+ALTER TABLE `tracks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
