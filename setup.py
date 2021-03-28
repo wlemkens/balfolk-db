991de79be862c71091f681bdf4fcfc99e8567910
Wim Lemkens <wim.lemkens@gmail.com>
2021-03-28 15:45:43 +0100

    Added images to build script and possibly made it compatible with linux

--- setup.py
+++ setup.py
@@ -2,13 +2,29 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = { "includes": ["tools","Music","tkinter"], "include_files": ["tools/ffmpeg.exe", "tools/ffprobe.exe", "tools/ffplay.exe"]}
+build_exe_options = {
+        "includes": ["tools","Music","tkinter"],
+        "include_files": [
+                          ("images/login.png", "images/login.png"),
+                          ("images/dances_local.png", "images/dances_local.png"),
+                          ("images/dances_remote.png", "images/dances_remote.png"),
+                          ("images/language.png", "images/language.png"),
+                          ("images/library.png", "images/library.png"),
+                          ("images/local_to_remote.png", "images/local_to_remote.png"),
+                          ("images/prepsync.png", "images/prepsync.png"),
+                          ("images/remote_to_local.png", "images/remote_to_local.png"),
+                          ("images/sync.png", "images/sync.png")
+                          ]
+}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
 base = None
-# if sys.platform == "win32":
-#    base = "Win32GUI"
+if sys.platform == "win32":
+   base = "Win32GUI"
+    build_exe_options["include_files"] += ["tools/ffmpeg.exe",
+                          "tools/ffprobe.exe",
+                          "tools/ffplay.exe"]
 
 
 setup(  name = "music_wizard",
